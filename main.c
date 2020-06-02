8bb16e2af88df331d48bf5b8acf90d6271b09190
VasyaSmolyar <smolyar4@gmail.com>
2020-06-02 12:39:11 +0300

    Расширил тестирование

--- main.c
+++ main.c
@@ -3,24 +3,38 @@
 
 int main() {
     HashTable table;
-    int a, b;
-    a = 100;
-    b = 51;
     HashCell cell1, cell2, *pcell1, *pcell2;
-    cell1.data = &a;
-    cell1.size = sizeof(a);
-    cell2.data = &b;
-    cell2.size = sizeof(b);
+    HashCell cell3, cell4, *pcell3, *pcell4;
+    int a = 100, b = 51;
+
+    hash_cell_create(&cell1, "Hello, world!", sizeof("Hello, world!"));
+    hash_cell_create(&cell2, "Goodby, world!", sizeof("Goodby, world!"));
+    hash_cell_create(&cell3, &a, sizeof(a));
+    hash_cell_create(&cell4, &b, sizeof(b));
     hash_table_create(&table, 10);
+
     hash_table_append(&table, &cell1);
     hash_table_append(&table, &cell2);
+    hash_table_append(&table, &cell3);
+    hash_table_append(&table, &cell4);
+
     pcell1 = hash_table_get_item(&table, hash_table_get_key(&table, &cell1));
     pcell2 = hash_table_get_item(&table, hash_table_get_key(&table, &cell2));
+    pcell3 = hash_table_get_item(&table, hash_table_get_key(&table, &cell3));
+    pcell4 = hash_table_get_item(&table, hash_table_get_key(&table, &cell4));
+
     if(pcell1 != NULL) {
-        printf("Cell 1 %d\n", *(int*)pcell1->data);
+        printf("Cell 1 %s\n", (char*)pcell1->data);
     }
     if(pcell2 != NULL) {
-        printf("Cell 2 %d\n", *(int*)(pcell2->data));
+        printf("Cell 2 %s\n", (char*)(pcell2->data));
     }
+    if(pcell3 != NULL) {
+        printf("Cell 3 %d\n", *(int*)pcell3->data);
+    }
+    if(pcell4 != NULL) {
+        printf("Cell 4 %d\n", *(int*)(pcell4->data));
+    }
+
     return 0;
 }
